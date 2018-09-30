f897ca7e375e09ae364530b777af6b25b6e76421
andersbor <anbo@easj.dk>
2018-09-30 14:12:46 +0200

    .gitignore updated

--- leaderboardRestProviderDatabaseVersion/Controllers/ConnectionstringHelper.cs
+++ leaderboardRestProviderDatabaseVersion/Controllers/ConnectionstringHelper.cs
@@ -9,7 +9,7 @@ namespace leaderboardRestProviderDatabaseVersion.Controllers
     public class ConnectionstringHelper
     {
         internal static string GetConnectionString()
-        {
+        { 
             return "Server=tcp:anbo-databaseserver.database.windows.net,1433;Initial Catalog=anbobase;Persist Security Info=False;User ID=anbo;Password=Hemmelig14;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             /*
             StreamReader reader = new StreamReader("connectionstring.txt");
